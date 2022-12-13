8b6afa1d3b80a0078b639b150506819bbeb5968e
Nisal Vimukthi <r.nisalvimukthi@gmail.com>
2022-12-13 22:05:23 +0530

    Add project files.

---
+++ WebApplication3/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication3.Models.Books> Books { get; set; }
+    }
+}
---
+++ WebApplication3/Models/Books.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication3.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public String BookName { get; set; }
+        public String BookAuthor { get; set; }
+
+        public Books()
+        {
+
+        }
+    }
+}
